cab88c80a3cfc30dc68487d85eba8a9d7d8fa76a
mohamed <mohamed.aden@live.ca>
2020-05-25 18:51:32 -0400

    Completed JS

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
 $(document).ready(function() {
-    let $removeSkill = $(".remove-skill")
-        //Removing a skill via button
-    $removeSkill.click(function() {
+    memo()
+    const $removeSkill = $(".remove-skill")
+    const $submitSkill = $(".submit-skill")
 
-        this.closest("tr").remove();
-        alert("Your skill has been removed")
+    /*--------------------
+       Add a skill memo 
+       ----------------------*/
+    function memo() {
+        if ($("table tr").length == 0) {
+            $("h2").css("display", "block")
+        } else {
+            $("h2").css("display", "none")
+        }
+    }
+
+    /*--------------------
+    Adding a new skill if 
+    theres input in the form 
+    ----------------------*/
+
+    $submitSkill.click(function() {
+        //grabs text
+        var newSkill = $("input").val()
+            //adds text and button to table
+        var addSkill = `<tr> <td><button class="remove-skill" type="button">Remove</button></td>
+       <td>${newSkill}</td></tr>`;
+        if (newSkill !== "") { //checks to make sure theres actual text 
+            $("table").append(addSkill)
+            memo()
+        }
     });
 
-    //Adding a new skill
-
-    $(".submit-skill").click(function() {
-        let newSkill = $("input").val()
-        let addSkill = `<tr> <td><button class="remove-skill" type="button">Remove</button></td>
-       <td>${newSkill}</td>
-     </tr>`;
-        $("table").append(addSkill)
+    /*--------------
+    Removing skill by 
+    clicking button 
+    ----------------*/
+    $('table').on('click', 'button', function() {
+        //fades it
+        $(this).closest('tr').fadeOut(500, function() {
+            //removes from the DOM
+            $(this).remove();
+            memo()
+        });
     });
-
 });
